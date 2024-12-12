a88c27c664eb004a79fa2f5ff7d36e4731dfb12c
Sam Lesser <ineedmoreplates@gmail.com>
2024-12-11 20:23:20 -0600

    create the twilio cred modal

---
+++ db/migrate/20241212022034_create_twilio_credentials.rb
@@ -0,0 +1,11 @@
+class CreateTwilioCredentials < ActiveRecord::Migration[6.1]
+  def change
+    create_table :twilio_credentials do |t|
+      t.string :account_sid
+      t.string :auth_token
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,16 @@
 
 
 #Users
-User.create(name: 'Jane Doe', email: 'jane_doe@example.com')
+user = User.create(name: 'Jane Doe', email: 'jane_doe@example.com')
 
 #Conversations
-Conversation.create(name: 'General Chat', user_ids: [user.id])
+conversation = Conversation.create(name: 'General Chat', user_ids: [user.id])
 
 #Message Model
+message = Message.create(user: user, conversation: conversation, body: "Hello, world!")
+
+#Twilio Credentials
+twilio_credentials = TwilioCredential.create(account_sid: "your_account_sid", auth_token: "your_auth_token", phone_number: "+1234567890")
 
 
 
