8b829e3a51eb2bd9034054aee7cee2a4a2fd8e18
Sam Lesser <ineedmoreplates@gmail.com>
2024-12-12 22:00:35 -0600

    update migrations, add factory bot, add user unit tests

--- db/migrate/20241213014845_create_join_table_conversations_users.rb
+++ db/migrate/20241213014845_create_join_table_conversations_users.rb
@@ -1,8 +1,13 @@
 class CreateJoinTableConversationsUsers < ActiveRecord::Migration[6.1]
   def change
-    create_join_table :conversations, :users do |t|
-      t.index :conversation_id
-      t.index :user_id
+    create_join_table :conversations, :users, id: false do |t|
+      # Adding indexes for performance
+      t.index [:user_id, :conversation_id], unique: true
+      t.index [:conversation_id, :user_id], unique: true
+      
+      # Adding foreign key constraints to ensure referential integrity
+      t.foreign_key :users
+      t.foreign_key :conversations
     end
   end
 end
