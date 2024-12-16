9d318edd811f6500c04b84f469d785d901907b23
Sam Lesser <ineedmoreplates@gmail.com>
2024-12-15 18:08:06 -0600

    add unit tests for message model + adjust configs for rails_helper.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
 
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 300 }
   validates :user, presence: true
 end
