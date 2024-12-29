10b7c056cf098b290ce44b33dbd36b7c1ed1bc6a
Sam Lesser <ineedmoreplates@gmail.com>
2024-12-29 11:48:16 -0600

    add join table messages_users, update models, refactor create method in messages controller, updated rspec test + associations, updated readme

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :conversations
-  has_many :messages
+  has_and_belongs_to_many :messages
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
