dfbbf381caf4f8da23a5048dee7a03b7642f2bea
Sam Lesser <ineedmoreplates@gmail.com>
2024-12-30 21:19:15 -0600

    setup test messaging and rspec mock test for twilio service

--- app/services/twilio_service.rb
+++ app/services/twilio_service.rb
@@ -20,4 +20,20 @@ class TwilioService
       raise
     end
   end
+
+  def send_test_message
+    # Use a valid phone number for testing.
+    test_phone_number = ENV['TEST_PHONE_NUMBER']
+    if test_phone_number.blank?
+      Rails.logger.error "Test phone number is missing in environment variables."
+      return
+    end
+
+    send_message(
+      from: ENV['TWILIO_PHONE_NUMBER'],
+      to: test_phone_number,
+      body: 'TEST MESSAGE: This is a test message from your Twilio integration.'
+    )
+  end
+
 end
