ab8b756696960dce235b295b80dfe811d9a72f42
Sam Lesser <ineedmoreplates@gmail.com>
2024-12-31 20:32:14 -0600

    setup base case routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
 
-  resources :conversations, only: [:show] do
-    get 'debug/chat/:id', to: 'chat#show', as: 'debug_chat'  #custom route for debugging chat in browser
+  root "conversations#index"
+
+  resources :conversations, only: [:index, :show, :create] do
+    resources :messages, only: [:create]
   end
 
-  mount ActionCable.server => '/cable'
+  resources :users, only: [:index, :show]
+
+  # Optional: If you want a debug route to list all conversations or messages for testing purposes
+  # get 'debug_chat', to: 'conversations#debug_chat'  # This would show all messages or data for debugging
 end
