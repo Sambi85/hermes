6c371a74dd58a1bc8a9df923fa96f8522fe4c5a1
Sam Lesser <ineedmoreplates@gmail.com>
2024-12-31 22:09:21 -0600

    finish adjusting tests for twilio services, update message model, update chat_channel.rb

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,26 +1,25 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    @conversation = Conversation.find(params[:conversation_id])
+    @conversation = Conversation.find_by(id: params[:conversation_id])
 
     if @conversation
       stream_for @conversation
     else
-      reject # Reject the connection if the conversation doesn't exist
+      reject_unauthorized_connection
     end
   end
 
   def unsubscribed
-    # Optionally, handle any cleanup here (e.g., notify other users that this user has left)
+    # Optionally handle cleanup when the user unsubscribes from the channel.
   end
 
   def send_message(data)
     if current_user
       message = @conversation.messages.create!(user: current_user, body: data['message'])
-      # Broadcast the message body and username to the channel
-      broadcast_to(@conversation, message: message.body, user: current_user.username)
+      
+      broadcast_to(@conversation, message: message.body, user: current_user.name)
     else
       reject
     end
   end
-
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -7,10 +7,6 @@ class Message < ApplicationRecord
   validates :user, presence: true
   before_validation :must_have_at_least_one_recipient
 
-  after_create_commit do
-    broadcast_to conversation, message: self.body
-  end
-
   def must_have_at_least_one_recipient
     if recipients.empty?
       errors.add(:recipients, "must have at least one recipient")
